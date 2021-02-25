f5b4c45dcecbea9b19257af21695d20d82856d79
Nicolas M <miller.nicolasw@gmail.com>
2021-02-24 17:02:30 -0800

    third

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
 #response = requests.get('https://pokeapi.co/api/')
 #data = response.json()
 #print(data)
+
+#https://pokeapi.co/api/v2/pokemon/{id or name}/
+#METHODS = stats, types, sprites, names
