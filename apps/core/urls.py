3f20a7f909ca8d7a46d2540855f5979b0db33fff
Nicolas M <miller.nicolasw@gmail.com>
2021-03-05 19:05:55 -0800

    data written, styling needed

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-import requests
 
 from apps.core import views
 
@@ -9,11 +8,3 @@ urlpatterns = [
     path('graph/', views.graph, name='graph'),
     path('chart/', views.chart, name='chart'),
 ]
-
-#response = requests.get('https://pokeapi.co/api/')
-#data = response.json()
-#print(data)
-
-#https://pokeapi.co/api/v2/pokemon/{id or name}/
-#METHODS = stats, types, sprites, names
-
--- apps/core/views.py
+++ apps/core/views.py
@@ -22,28 +22,33 @@ def home(request):
 
 
 def graph(request):
+    line_chart = pygal.HorizontalBar()
+    line_chart.title = 'Breloom stat spread'
+    for stat_key in breloom_stats:
+        value = stat_key['base_stat']
+        label = stat_key['stat']['name']
+        line_chart.add(label, value)
+
+    chart_svg = line_chart.render()
+
     context = {
-        'pokemon': pokepy.V2Client().get_pokemon('breloom'),
+        'pokemon': breloom_name.upper(),
         'stats': breloom_stats,
+        "rendered_chart_svg": chart_svg,
+        'sprite': breloom_sprite,
     }
     return render(request, 'pages/graph.html', context)
 
 
 def chart(request):
+    stats_chart = pygal.Pie()
+    stats_chart.title = "Breloom stat spread (in %)"
+    for stat_key in breloom_stats:
+        value = stat_key['base_stat']
+        label = stat_key['stat']['name']
+        stats_chart.add(label, value)
 
-
-    #stat_list = []
-    #for i in range(6):
-    #    stat_list += breloom_stats['stats'][str(i)]['name']
-
-    #stats_chart = pygal.Pie()
-    #stats_chart.title = "Breloom's stat spread (in %)"
-    #for repo_dict in repo_list:
-    #    value = 42 # TODO: Replace this...
-    #    label = repo_dict["name"]
-    #    chart.add(label, value)
-
-    #chart_svg = stats_chart.render()
+    chart_svg = stats_chart.render()
 
     context = {
         'pokemon': breloom_name.upper(),
